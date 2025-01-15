c82fd0c83acdbec8769eabb99b7b7aad47c5e01a
hnamredips <nghnam265@gmail.com>
2025-01-15 22:39:45 +0700

    UI Blog and BlogDetails

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Blog from "./pages/Blog";
+import BlogDetail from "./pages/BlogDetail";
 import Community from "./pages/Community";
 import Service from "./pages/Service";
 import Contact from "./pages/Contact";
@@ -17,7 +18,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
-          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/" element={<Blog />} />
+          <Route path="/post/:id" element={<BlogDetail />} />
           <Route path="/community" element={<Community />} />
           <Route path="/service" element={<Service />} />
           <Route path="/contact" element={<Contact />} />
