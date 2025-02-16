c8c7658aaf577608ce10cc53c6b9540f22e91763
Phạm Mạnh Cường <121109874+PMCdaniel123@users.noreply.github.com>
2025-02-16 13:32:47 +0700

    Cuong (#10)
    * chat Gemini
    
    * fix css
    
    * sửa nội dung
    
    * mobile layout
    
    * member avatar

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -14,11 +14,13 @@ function Book({ index, image, title, author, price, hot }) {
           className="w-full h-96 object-cover rounded-t-lg"
         />
         <div className="p-4 flex flex-col">
+          <div className="flex items-center mb-2">
+            <p className="text-sm font-medium text-white bg-green-700 py-1 px-4 rounded-xl">
+              {price}
+            </p>
+          </div>
           <h2 className="mb-2 font-bold text-gray-800 text-xl">{title}</h2>
           <p className="text-gray-600 mb-1">{author}</p>
-          <div className="flex items-center gap-2">
-            <p className="text-lg font-bold text-green-700">{price}VND</p>
-          </div>
         </div>
         <div className="absolute inset-0 bg-gradient-to-b from-green-300 via-transparent to-transparent opacity-0 group-hover:opacity-80 transition-opacity duration-300 ease-in-out rounded-lg"></div>
       </div>
